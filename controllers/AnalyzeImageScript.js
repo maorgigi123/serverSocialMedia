88716070001fe35f925c7b650c800de60a1b3804
maorgigi123 <maorgigim1509@gmail.com>
2024-08-28 21:45:12 +0300

    aaa

--- controllers/AnalyzeImageScript.js
+++ controllers/AnalyzeImageScript.js
@@ -1,45 +1,46 @@
-const tf = require('@tensorflow/tfjs');
-const cocoSsd = require('@tensorflow-models/coco-ssd');
-const { createCanvas, loadImage } = require('canvas');
-const fs = require('fs');
+// const tf = require('@tensorflow/tfjs');
+// const cocoSsd = require('@tensorflow-models/coco-ssd');
+// const { createCanvas, loadImage } = require('canvas');
+// const fs = require('fs');
 
-const modelPromise = cocoSsd.load();
-let tags = []
-async function analyzeImage(imagePath) {
-    try{
-        const model = await modelPromise;
-    const image = await loadImage(imagePath);
-    const canvas = createCanvas(image.width, image.height);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0, image.width, image.height);
+// const modelPromise = cocoSsd.load();
+// let tags = []
+// async function analyzeImage(imagePath) {
+//     try{
+//         const model = await modelPromise;
+//     const image = await loadImage(imagePath);
+//     const canvas = createCanvas(image.width, image.height);
+//     const ctx = canvas.getContext('2d');
+//     ctx.drawImage(image, 0, 0, image.width, image.height);
     
-    const predictions = await model.detect(canvas);
-    const tagsSet = new Set();
+//     const predictions = await model.detect(canvas);
+//     const tagsSet = new Set();
 
-    predictions.forEach(prediction => {
-        console.log(`Detected ${prediction.class} with ${Math.round(prediction.score * 100)}% confidence.`);
-        tagsSet.add(prediction.class);
-        // Save or use the prediction data
-    });
-    tags =  Array.from(tagsSet)
+//     predictions.forEach(prediction => {
+//         console.log(`Detected ${prediction.class} with ${Math.round(prediction.score * 100)}% confidence.`);
+//         tagsSet.add(prediction.class);
+//         // Save or use the prediction data
+//     });
+//     tags =  Array.from(tagsSet)
      
-    return predictions;
-    }catch(e){
-        console.log('Error analyzing image for',imagePath)
-    }
-}
+//     return predictions;
+//     }catch(e){
+//         console.log('Error analyzing image for',imagePath)
+//     }
+// }
 
-// analyzeImage('/Users/maorgigi/Desktop/ServerSocialMedia/uploads/maor gigi/Sss/30.07.24-16-07-26/cover.png').then(predictions => {
-//     console.log(tags)
-// });
+// // analyzeImage('/Users/maorgigi/Desktop/ServerSocialMedia/uploads/maor gigi/Sss/30.07.24-16-07-26/cover.png').then(predictions => {
+// //     console.log(tags)
+// // });
 
 const AnalyzeImageScript = async(pathImage) => {
-    return await analyzeImage(pathImage).then(predictions => {
-        return tags.flat()
-    }).catch((error) =>{
-        console.log(error)
-        return []
-    })
+    // return await analyzeImage(pathImage).then(predictions => {
+    //     return tags.flat()
+    // }).catch((error) =>{
+    //     console.log(error)
+    //     return []
+    // })
+    return tags.flat()
 }
 
 module.exports = AnalyzeImageScript;
--- controllers/analyzeVideoScript.js
+++ controllers/analyzeVideoScript.js
@@ -1,87 +1,87 @@
-const tf = require('@tensorflow/tfjs');
-const cocoSsd = require('@tensorflow-models/coco-ssd');
-const { createCanvas, loadImage } = require('canvas');
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
+// const tf = require('@tensorflow/tfjs');
+// const cocoSsd = require('@tensorflow-models/coco-ssd');
+// const { createCanvas, loadImage } = require('canvas');
+// const fs = require('fs');
+// const ffmpeg = require('fluent-ffmpeg');
+// const path = require('path');
 
 // טעינת המודל
-const modelPromise = cocoSsd.load();
-let tags = []
-// פונקציה לניתוח תמונה
-async function analyzeImage(imagePath) {
-    const model = await modelPromise;
-    const image = await loadImage(imagePath);
-    const canvas = createCanvas(image.width, image.height);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0, image.width, image.height);
+// const modelPromise = cocoSsd.load();
+// let tags = []
+// // פונקציה לניתוח תמונה
+// async function analyzeImage(imagePath) {
+//     const model = await modelPromise;
+//     const image = await loadImage(imagePath);
+//     const canvas = createCanvas(image.width, image.height);
+//     const ctx = canvas.getContext('2d');
+//     ctx.drawImage(image, 0, 0, image.width, image.height);
     
-    const predictions = await model.detect(canvas);
-    const tagsSet = new Set();
+//     const predictions = await model.detect(canvas);
+//     const tagsSet = new Set();
 
-    predictions.forEach(prediction => {
-        // console.log(`Detected ${prediction.class} with ${Math.round(prediction.score * 100)}% confidence.`);
-        tagsSet.add(prediction.class);
-    });
+//     predictions.forEach(prediction => {
+//         // console.log(`Detected ${prediction.class} with ${Math.round(prediction.score * 100)}% confidence.`);
+//         tagsSet.add(prediction.class);
+//     });
     
-    tags =  Array.from(tagsSet)
-    return predictions;
-}
+//     tags =  Array.from(tagsSet)
+//     return predictions;
+// }
 
-// פונקציה לחילוץ פריימים מווידאו
-function extractFrames(videoPath, outputDir) {
-    return new Promise((resolve, reject) => {
-        ffmpeg(videoPath)
-            .on('end', resolve)
-            .on('error', reject)
-            .screenshots({
-                count: 10,
-                folder: outputDir,
-                size: '320x240'
-            });
-    });
-}
+// // פונקציה לחילוץ פריימים מווידאו
+// function extractFrames(videoPath, outputDir) {
+//     return new Promise((resolve, reject) => {
+//         ffmpeg(videoPath)
+//             .on('end', resolve)
+//             .on('error', reject)
+//             .screenshots({
+//                 count: 10,
+//                 folder: outputDir,
+//                 size: '320x240'
+//             });
+//     });
+// }
 
-// פונקציה לניתוח ווידאו
-async function analyzeVideo(videoPath,username,imageName) {
-    const framesDir = path.join(__dirname, 'temp', username, imageName);
+// // פונקציה לניתוח ווידאו
+// async function analyzeVideo(videoPath,username,imageName) {
+//     const framesDir = path.join(__dirname, 'temp', username, imageName);
 
-    try {
-        await fs.mkdirSync(framesDir, { recursive: true });
-    } catch (error) {
-        console.error(`Failed to create directory ${framesDir}:`, error);
-    }
-    await extractFrames(videoPath, framesDir);
+//     try {
+//         await fs.mkdirSync(framesDir, { recursive: true });
+//     } catch (error) {
+//         console.error(`Failed to create directory ${framesDir}:`, error);
+//     }
+//     await extractFrames(videoPath, framesDir);
     
-    const files = fs.readdirSync(framesDir);
-    for (const file of files) {
-        const filePath = path.join(framesDir, file);
-        const predictions = await analyzeImage(filePath);
-        // Process predictions
-    }
+//     const files = fs.readdirSync(framesDir);
+//     for (const file of files) {
+//         const filePath = path.join(framesDir, file);
+//         const predictions = await analyzeImage(filePath);
+//         // Process predictions
+//     }
 
-    return tags
-}
+//     return tags
+// }
 
-// פונקציה למחיקת תיקיה ותוכנה
-function deleteFolderRecursive(folderPath) {
-    if (fs.existsSync(folderPath)) {
-        fs.rmSync(folderPath, { recursive: true, force: true });
-        // console.log(`Deleted folder: ${folderPath}`);
-    } else {
-        console.log(`Folder not found: ${folderPath}`);
-    }
-}
+// // פונקציה למחיקת תיקיה ותוכנה
+// function deleteFolderRecursive(folderPath) {
+//     if (fs.existsSync(folderPath)) {
+//         fs.rmSync(folderPath, { recursive: true, force: true });
+//         // console.log(`Deleted folder: ${folderPath}`);
+//     } else {
+//         console.log(`Folder not found: ${folderPath}`);
+//     }
+// }
 // קריאה לפונקציה לניתוח ווידאו
 
 const analyzeVideoScript = async(pathVideo, username,imageName) => {
-    return await analyzeVideo(pathVideo,username,imageName).then((data) => {
-        deleteFolderRecursive(path.join(__dirname, 'temp', username, imageName))
-        return tags.flat();
-    }).catch((error) =>{
-        console.log(error)
-    })
-    
+    // return await analyzeVideo(pathVideo,username,imageName).then((data) => {
+    //     deleteFolderRecursive(path.join(__dirname, 'temp', username, imageName))
+    //     return tags.flat();
+    // }).catch((error) =>{
+    //     console.log(error)
+    // })
+    return tags.flat();
 }
 
 module.exports = analyzeVideoScript;
